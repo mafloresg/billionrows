7456e80dc4bb3e482d90ae0654976fe51a9af888
Maria AF <m.angeles.flores@gmail.com>
2024-03-22 12:08:33 +0000

    Testing a simple function failing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world! 6");
+    println!("{result}", result = add(1,2));
+}
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b + 1
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1,2), 3);
+    }
 }
